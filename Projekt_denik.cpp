9382b9922c70dbad912143e0205b257f19163ca8
Prochimar <prochimar@seznam.cz>
2025-10-14 14:29:50 +0200

    prvni komit

---
+++ Projekt_denik.cpp
@@ -0,0 +1,12 @@
+﻿// Projekt_denik.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "Projekt_denik.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
